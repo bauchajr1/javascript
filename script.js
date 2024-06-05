fe4f7a00d0bd44f80748bdfb29221be47a9b2900
bauchajr1 <132862158+bauchajr1@users.noreply.github.com>
2024-06-05 09:27:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let human_name  =prompt("enter your name")
+document.getElementById('name').innerHTML ="welcome "+ human_name
+
+let address = prompt("enter your address")
+document.getElementById('address').innerHTML ="i live in " + address
+
+let age = prompt("enter your age")
+document.getElementById('age').innerHTML ="i am " + age + " years old"
+
